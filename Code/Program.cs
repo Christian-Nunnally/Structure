f3344e95141d14f82e930d2cb0e3fd5e2207fa76
Christian Nunnally <christiansnunnally@gmail.com>
2020-10-19 20:20:57 -0500

    test

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,72 +1,21 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Threading;
 
-namespace Structure
+namespace Structure.Code
 {
     internal class Program
     {
-        public const string SaveFileExtension = ".structure";
-        public static string SaveDirectory = null;
-
-        public static int GetValue(string key)
-        {
-            var fileName = GetSaveFileName(key);
-            return File.Exists(fileName) && int.TryParse(File.ReadAllText(fileName), out var points) ? points : 0;
-        }
-
-        public static void SetValue(string key, int value)
-        {
-            string fileName = GetSaveFileName(key);
-            File.WriteAllText(fileName, value.ToString());
-        }
-
-        public static int GetDebt() => GetValue("debt");
-
-        public static int GetPoints() => GetValue("points");
-
-        public static void SetPoints(int points) => SetValue("points", points);
-
-        public static void SetDebt(int debt) => SetValue("debt", debt);
-
-        public static void SetLevel(int level) => SetValue("level", level);
-
-        private static string GetSaveFileName(string key)
-        {
-            if (string.IsNullOrEmpty(SaveDirectory))
-            {
-                var appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-                var structureSettingsDirectory = $"{appDataFolder}\\Structure\\";
-                var structureSettingsPath = $"{structureSettingsDirectory}\\settings.structure";
-                if (!File.Exists(structureSettingsPath))
-                {
-                    Console.WriteLine("Enter a path for your structure save files to live:");
-                    var path = Console.ReadLine();
-                    path = path.EndsWith("//") ? path : $"{path}\\";
-                    Directory.CreateDirectory(structureSettingsDirectory);
-                    File.WriteAllText(structureSettingsPath, path);
-                }
-                SaveDirectory = File.ReadAllText(structureSettingsPath);
-            }
-            return $"{SaveDirectory}{key}{SaveFileExtension}";
-        }
-
         private static void Main(string[] args)
         {
-            var level = GetValue("level");
-            var xpForNextLevel = ExperienceForLevel(level + 1);
-            string debtFileName = GetSaveFileName("debt");
-            if (File.GetLastWriteTime(debtFileName).Date != DateTime.Today.Date)
-            {
-                SetDebt(GetDebt() + GetCodeLength());
-            }
+            var level = Data.GetLevel();
+            var xpForNextLevel = Utility.ExperienceForLevel(level + 1, 10, 150, 13);
+            var codeLength = Utility.GetCodeLength();
 
-            var codeLength = GetCodeLength();
-            Console.WriteLine("Code length = " + codeLength);
+            IncrementDebt(codeLength);
+
             while (true)
             {
-                var points = GetPoints();
+                var points = Data.GetPoints();
                 Console.WriteLine($"Progress: {points}/{xpForNextLevel}");
                 Console.WriteLine("Type task and press enter when complete.");
                 var task = Console.ReadLine();
@@ -76,7 +25,7 @@ namespace Structure
                 Console.WriteLine("Point gained!");
                 Thread.Sleep(500);
                 points++;
-                SetPoints(points);
+                Data.SetPoints(points);
                 Console.Clear();
                 if (points >= xpForNextLevel)
                 {
@@ -85,31 +34,22 @@ namespace Structure
                     if (result == "y")
                     {
                         points -= xpForNextLevel;
-                        SetPoints(points);
+                        Data.SetPoints(points);
                         level++;
-                        SetLevel(level);
+                        Data.SetLevel(level);
                         break;
                     }
                 }
             }
         }
 
-        private static int GetCodeLength()
+        private static void IncrementDebt(int codeLength)
         {
-            var temp = Directory.GetFiles(".");
-            var allCSFiles = Directory.GetFiles(@"C:\Users\chris\source\repos\Structure\Structure\Code", "*.cs", new EnumerationOptions() { RecurseSubdirectories = true });
-            return allCSFiles.Sum(f => File.ReadAllText(f).Length);
-        }
-
-        private static int ExperienceForLevel(int level)
-        {
-            double total = 0;
-            for (int i = 1; i < level; i++)
+            Console.WriteLine("Added debt per day = " + codeLength);
+            if (Data.GetLastWriteTime("debt").Date != DateTime.Today.Date)
             {
-                total += Math.Floor(i + 150 * Math.Pow(2, i / 13.0));
+                Data.SetDebt(Data.GetDebt() + codeLength);
             }
-
-            return (int)Math.Floor(total / 4);
         }
     }
 }
