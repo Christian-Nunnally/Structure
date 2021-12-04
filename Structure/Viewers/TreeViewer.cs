9234d306ed04123329fa193b9b7cee95d1bde817
Christian Nunnally <christiansnunnally@gmail.com>
2021-12-04 13:36:01 -0600

    Finished organizing files

--- Structure/Viewers/TreeViewer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Structure.Code.Viewers
-{
-    public class TreeViewer : TreeEditor<Node>
-    {
-        public TreeViewer(string prompt, PersistedTree<Node> tree) : base(prompt, tree)
-        {
-        }
-    }
-}
